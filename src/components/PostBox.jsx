b3016f5d1fc4a4901c5ea91ee34570a2e675c4ae
jefti <jeftimeira@gmail.com>
2024-01-10 16:17:10 -0300

    feat: Initialize PostContainer with static layout

---
+++ src/components/PostBox.jsx
@@ -0,0 +1,84 @@
+import styled from "styled-components"
+
+export default function PostBox(){
+    return (
+    <PostContainer>
+        <ImageContainer>
+            <img src="https://picsum.photos/200/300" alt="foto de perfil"/>
+        </ImageContainer>
+        <ContentContainer>
+            <TitleContainer>What are you going to share today?</TitleContainer>
+            <StyledInput placeholder="http://..." />
+            <StyledTextArea tall placeholder="Awesome article about #javascript" />
+            <button>publish</button>
+        </ContentContainer>
+    </PostContainer>)
+}
+
+const PostContainer = styled.div`
+    width: 610px;
+    height: 210px;
+    border-radius: 15px;
+    padding: 15px;
+    background-color: red;
+    display: flex;
+    flex-direction: row;
+    gap: 15px;
+`
+
+const ImageContainer = styled.div`
+    width: 50px;
+    img{
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    }
+`
+
+const ContentContainer = styled.div`
+    background-color: blue;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+
+const TitleContainer = styled.div`
+    color: #707070;
+    font-weight: 300;
+    font-size: 20px;
+    line-height: 24px;
+`
+
+const StyledInput = styled.input`
+    width: 503px;
+    height: 30px;
+    background-color: #EFEFEF;
+    border: none;
+    border-radius: 5px;
+    padding-left: 10px;
+    ::placeholder {
+        font-size: 15px;
+        font-weight: 300;
+        line-height: 18px;
+        color: #999;
+        font-style: italic;
+    }
+`
+
+const StyledTextArea = styled.textarea`
+    width: 503px;
+    height: 66px;
+    background-color: #EFEFEF;
+    border: none;
+    border-radius: 5px;
+    padding-left: 10px;
+    resize: none;
+    ::placeholder {
+        font-size: 15px;
+        font-weight: 300;
+        line-height: 18px;
+        color: #999;
+        font-style: italic;
+    }
+`
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-    </div>
-  );
-}
+import PostBox from "../components/PostBox";
 
+export default function HomePage(){
+    return (
+        <>
+            <PostBox/>
+        </>
+    )
+}
